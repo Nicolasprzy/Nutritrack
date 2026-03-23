a52e5d99557227f9433fbbf98ab88bb3bd4158a5
Nicolas <przymusinski.n@icloud.com>
2026-03-23 14:03:33 +0100

    Initial Commit

---
+++ NutriTrack/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  NutriTrack
+//
+//  Created by Nicolas on 23/03/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ NutriTrack/NutriTrackApp.swift
@@ -0,0 +1,32 @@
+//
+//  NutriTrackApp.swift
+//  NutriTrack
+//
+//  Created by Nicolas on 23/03/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NutriTrackApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
